f528e561a1ca713e2126faeac6d330ca948a4380
pancx <p_lord@163.com>
2024-07-17 11:16:30 +0800

    ✨ feat(common.sh): 增加日志打印功能
    1. 增加日志打印函数，包括4个等级（debug、info、warn、error），可以设置日志输出颜色，不同等级的日志信息颜色不同。
    2. Python绑定函数增加寻找python3的逻辑

--- functions/common.sh
+++ functions/common.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# 所需 python 版本
+PYTHON_VERSION="3.11.5"
+# 控制日志是否显示颜色
+LOGGING_COLOR_ENABLE=true
+# 是否开启 DEBUG 日志
+DEBUG_ENABLE=true
+# 定义颜色
+# 红色，对应ERROR、WARN等级
+RED='\033[0;31m'
+# 没有颜色，对应INFO等级
+NC='\033[0m'
+# 青色，对应DEBUG等级
+CYAN='\033[0;36m'
+
 # 打印带边框的消息
 # Usage: get_decorated_info <info> <fill_char>
 get_decorated_info() {
@@ -21,10 +35,86 @@ get_decorated_info() {
     echo "${line} ${message} ${line}"
 }
 
-# 打印日志
-# Usage: func_logging <content>
-function func_logging() {
+# 打印 DEBUG 日志
+debug_logging() {
     if [ $# -lt 1 ]; then
+        echo "Usage: $0 <content>"
+        exit 1
+    fi
+
+    # 允许 DEBUG 日志才会打印
+    if [ $DEBUG_ENABLE = true ]; then
+        func_logging "DEBUG" "$@"
+    fi
+}
+
+# 打印 INFO 日志
+info_logging() {
+    if [ $# -lt 1 ]; then
+        echo "Usage: $0 <content>"
+        exit 1
+    fi
+
+    func_logging "INFO" "$@"
+}
+
+# 打印 WARN 日志
+warn_logging() {
+    if [ $# -lt 1 ]; then
+        echo "Usage: $0 <content>"
+        exit 1
+    fi
+
+    func_logging "WARN" "$@"
+}
+
+# 打印 ERROR 日志
+error_logging() {
+    if [ $# -lt 1 ]; then
+        echo "Usage: $0 <content>"
+        exit 1
+    fi
+
+    func_logging "ERROR" "$@"
+}
+
+# 打印日志
+# Usage: func_logging <level> <content>
+function func_logging() {
+
+    if [ $# -lt 1 ]; then
+        echo "Usage: $0 <level> <content>"
+        exit 1
+    fi
+
+    # 获取日志等级
+    level="$1"
+    if [ "$LOGGING_COLOR_ENABLE" = true ]; then
+        case $level in
+        "DEBUG")
+            level_color="$CYAN"
+            ;;
+        "INFO")
+            level_color="$NC"
+            ;;
+        "WARN")
+            level_color="$RED"
+            ;;
+        "ERROR")
+            level_color="$RED"
+            ;;
+        *)
+            echo "Unknown level: $level" >&2
+            exit 1
+            ;;
+        esac
+    else
+        level_color="$NC"
+    fi
+    shift
+
+    # 获取日志信息
+    if [ $# -lt 2 ]; then
         content='Warning-Content-Missing'
     else
         content_array=("$@")
@@ -32,7 +122,7 @@ function func_logging() {
     fi
 
     file_name=$(basename "$0")
-    echo "file_name: $file_name | line: $LINENO | user: $(whoami) | host: $(hostname) | time: $(date +'%Y-%m-%d %H:%M:%S') -  ${content}"
+    echo "file_name: $file_name | level: ${level} | user: $(whoami) | host: $(hostname) | time: $(date +'%Y-%m-%d %H:%M:%S') -  ${level_color}${content}${NC}"
 }
 
 # 绑定java到JAVA_RUN
@@ -53,9 +143,10 @@ bind_java_run() {
 }
 
 # 绑定python到PYTHON_RUN
+# Usage: bind_python_run
 bind_python_run() {
     # 通过Python_Home环境变量查找Python
-    PYTHON_HOME="${PYTHON_HOME:-"/usr/local/python"}"
+    PYTHON_HOME="${PYTHON_HOME:-"$PYTHONHOME"}"
     if [ -n "$PYTHON_HOME" ] && [ -f "$PYTHON_HOME"/bin/python ]; then
         PYTHON_RUN="${PYTHON_HOME}/bin/python"
     fi
@@ -63,6 +154,11 @@ bind_python_run() {
     # 如果还没有找到Python, 则通过which查找python指令并bind到PYTHON_RUN
     if [ -z "$PYTHON_RUN" ]; then
         PYTHON_RUN=$(which python 2>/dev/null)
+    fi
+
+    # 如果还没有找到Python, 则通过which查找python3指令并bind到PYTHON_RUN
+    if [ -z "$PYTHON_RUN" ]; then
+        PYTHON_RUN=$(which python3 2>/dev/null)
     fi
 
     # 如果还没有找到Python,使用conda查找python指令并bind到PYTHON_RUN
