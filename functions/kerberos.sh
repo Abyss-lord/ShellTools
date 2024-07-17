9e261261de40a73b02073742ddc59c8309cd0289
pancx <p_lord@163.com>
2024-07-17 11:47:43 +0800

    🦄 refactor(kerberos.sh): 更新kerberos逻辑
    1. 脚本增加参数验证，重构keytabs_dir的获取逻辑；
    2. 弃用旧的echo日志体系，采用common.sh中新的日志函数；

--- functions/kerberos.sh
+++ functions/kerberos.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
+
+BIN_DIR=$(dirname "$0")
+BIN_DIR=$(
+    cd "$BIN_DIR" || exit
+    pwd
+)
+. "${BIN_DIR}/common.sh"
+
 # 刷新kerberos票据
+# refresh_kerberos_principal <user> [keytab_dir]
 refresh_kerberos_principal() {
-    # refresh_kerberos_principal <user> <keytab_dir>
-    message=$(get_decorated_info "Start Authenticate!")
-    echo "${message}"
-    kerberos_name="$1"
-    keytabs_dir="$2"
-    if [ -z "$keytabs_dir" ]; then
-        keytabs_dir="/etc/security/keytabs"
+
+    if [ $# -lt 1 ]; then
+        echo "Usage: refresh_kerberos_principal <user> [keytab_dir]"
+        exit 1
     fi
 
-    principal=$(klist -kt "${keytabs_dir}"/"${kerberos_name}".keytab | grep "${kerberos_name}"/ | awk '{print $4}' | tail -n 1)
-    echo "Use Principal = ${principal}"
-    if [ -z "$principal" ]; then
-        echo "principal is empty"
+    user_name="$1"
+
+    if [ "$#" -gt 1 ]; then
+        keytabs_dir="$2"
+    fi
+
+    keytabs_dir="${keytabs_dir:-/etc/security/keytabs}"
+
+    if [ -d "$keytabs_dir" ]; then
+        error_logging "keytabs_dir is empty"
         exit 126
     fi
-    kinit -kt "${keytabs_dir}"/"${kerberos_name}".keytab "${principal}"
+
+    # 开始认证
+    message=$(get_decorated_info "Start Authenticate!")
+    echo "${message}"
+
+    principal=$(klist -kt "${keytabs_dir}"/"${user_name}".keytab | grep "${user_name}"/ | awk '{print $4}' | tail -n 1)
+    echo "Use Principal = ${principal}"
+    if [ -z "$principal" ]; then
+        error_logging "principal is empty"
+        exit 126
+    fi
+    # 刷新票据
+    kinit -kt "${keytabs_dir}"/"${user_name}".keytab "${principal}"
 
     message=$(get_decorated_info "Success Authenticate!")
     echo "${message}"
